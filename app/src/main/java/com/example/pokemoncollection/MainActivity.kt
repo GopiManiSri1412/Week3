cb8e1923b8b0d8739c27dbd04c7affc1deb81f1c
d3667509 <D3667509@live.tees.ac.uk>
2024-10-08 17:17:33 +0100

    Chabge the name and commit

--- app/src/main/java/com/example/pokemoncollection/MainActivity.kt
+++ app/src/main/java/com/example/pokemoncollection/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             PokemonCollectionTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Gopi Mani Sri",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
